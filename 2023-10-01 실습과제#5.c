554e3723d94191defa4ec8840e86bb99a31e4a2e
shin-yeseong <144222508+shin-yeseong@users.noreply.github.com>
2023-10-01 13:55:06 +0000

    Update 2023-10-01 실습과제#5.c

--- 2023-10-01 실습과제#5.c
+++ 2023-10-01 실습과제#5.c
@@ -11,8 +11,12 @@ int main() {
     printf("학번을 입력하세요: ");
     scanf("%d", &student_id);
 
+    printf("학과를 입력하세요: ");
+    scanf("%s", &dp);
+
     printf("이름: %s\n", name);
     printf("학번: %d\n", student_id);
+    printf("학과: %s\n", dp);
 
     return 0;
 }
