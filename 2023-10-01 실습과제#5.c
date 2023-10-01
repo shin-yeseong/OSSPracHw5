d4abe86f8736014a5d5919bcaa578ca4a3a00899
shin-yeseong <144222508+shin-yeseong@users.noreply.github.com>
2023-10-01 14:09:45 +0000

    Update 2023-10-01 실습과제#5.c

--- 2023-10-01 실습과제#5.c
+++ 2023-10-01 실습과제#5.c
@@ -1,9 +1,9 @@
-/main
 #include <stdio.h>
 
 int main() {
     char name[100];
     int student_id;
+    char department[100];
 
     printf("이름을 입력하세요: ");
     scanf("%s", name);
@@ -12,11 +12,11 @@ int main() {
     scanf("%d", &student_id);
 
     printf("학과를 입력하세요: ");
-    scanf("%s", &dp);
+    scanf("%s", department);
 
     printf("이름: %s\n", name);
     printf("학번: %d\n", student_id);
-    printf("학과: %s\n", dp);
+    printf("학과: %s\n", department);
 
     return 0;
 }
