c3e9ec6784db7e45f5e819151f4c0acef941416b
shin-yeseong <144222508+shin-yeseong@users.noreply.github.com>
2023-10-08 11:32:22 +0000

    Update 개인과제_6_신예성.py

--- 개인과제_6_신예성.py
+++ 개인과제_6_신예성.py
@@ -3,14 +3,14 @@ def main():
     name = input("이름을 입력하세요: ")
     student_id = input("학번을 입력하세요: ")
     department = input("학과를 입력하세요: ")
-    school = input("학교를 입력하세요: ")
+    grade = input("학년을 입력하세요: ")
 
     # 학생 정보 출력
     print("\n입력한 학생 정보:")
     print("이름:", name)
     print("학번:", student_id)
     print("학과:", department)
-    print("학교:", school)
+    print("학년:", grade)
 
 if __name__ == "__main__":
     main()
